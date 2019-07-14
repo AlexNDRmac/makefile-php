0a80edda66ca8c48908b5e0fba95dc9e040d4c71
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-07-14 20:52:00 +0300

    refactor colors, add internal descriptions

--- .makefiles/colors.mk
+++ .makefiles/colors.mk
@@ -1,43 +1,45 @@
 ############################################
 # Colors
 ############################################
+# Color  Foreground  Background
+# black     30          40
+# red       31          41
+# green     32          42
+# yellow    33          43
+# blue      34          44
+# magenta   35          45
+# cyan      36          46
+# white     37          47
 
 # Regular Colors
-NC         =\033[0m
-Black      =\033[0;30m
-Red        =\033[0;31m
-Green      =\033[0;32m
-Yellow     =\033[0;33m
-Blue       =\033[0;34m
-Purple     =\033[0;35m
-Cyan       =\033[0;36m
-White      =\033[0;37m
-Gray       =\033[0;90m
+NC         = \033[0m
+Black      = \033[0;30m
+Red        = \033[1;31m
+Green      = \033[1;32m
+Yellow     = \033[1;33m
+Blue       = \033[1;34m
+Purple     = \033[1;35m
+Cyan       = \033[1;36m
+White      = \033[1;37m
+Gray       = \033[1;90m
 
 # Background Colors
-On_Black   =\033[40m
-On_Red     =\033[41m
-On_Green   =\033[42m
-On_Yellow  =\033[43m
-On_Blue    =\033[44m
-On_Purple  =\033[45m
-On_Cyan    =\033[46m
-On_White   =\033[47m
+On_Black   = \033[40m
+On_Red     = \033[41m
+On_Green   = \033[42m
+On_Yellow  = \033[43m
+On_Blue    = \033[44m
+On_Purple  = \033[45m
+On_Cyan    = \033[46m
+On_White   = \033[47m
 
-OK_COLOR    = \033[32;01m
-ERROR_COLOR = \033[31;01m
-WARN_COLOR  = \033[33;01m
-
-solid_line = ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-dash_line  = ------------------------------------------------------------------------
-dots_line  = ........................................................................
+solid_line  = ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+dash_line   = ------------------------------------------------------------------------
+dots_line   = ........................................................................
 ascii_block = ██████████
 
 .PHONY: show_colors
 show_colors:
-	@echo "OK_COLOR:    $(OK_COLOR)$(ascii_block)$(NC)\n"
-	@echo "WARN_COLOR:  $(WARN_COLOR)$(ascii_block)$(NC)\n"
-	@echo "ERROR_COLOR: $(ERROR_COLOR)$(ascii_block)$(NC)\n"
 	@echo "Black:       $(Black)$(ascii_block)$(NC)\n"
 	@echo "Red:         $(Red)$(ascii_block)$(NC)\n"
 	@echo "Green:       $(Green)$(ascii_block)$(NC)\n"
