1860cca455b9c3d21e78cd97fd9a193f91766630
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-03-16 14:34:23 +0200

    Add tiny small php makefile

--- README.md
+++ README.md
@@ -1,48 +1,36 @@
-# Makefile-php
+# PHP Makefile
 
 [![CircleCI](https://img.shields.io/circleci/build/github/AlexNDRmac/makefile-php?style=flat-square)](https://circleci.com/gh/AlexNDRmac/makefile-php)
 
-## Makefile for Laravel projects
+## Makefile for common PHP projects
 
-This Makefile helps PHP developers with runnig all necessary php tools as simple as possible. You can use this Makefile on local machine and various CI.
+This Makefile helps PHP developers with running all necessary PHP tools as simple as possible. You can use this Makefile on local machine and various CI and with PHPStorm as `Run configurations`
 
-You don't need to deeply know how to configure all Tools for Laravel project, Makefile already have configuration Templates for all Tools (of course you can modify it if you wish).
+You don't need to deeply know how to configure all Tools for Laravel or any other project, Makefile already have configuration Templates for all Tools (of course you can modify it if you wish).
 
-```bash
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-            __  ___      __        _____ __           ____  __  ______ 
-           /  |/  /___ _/ /_____  / __(_) /__        / __ \/ / / / __ \
-          / /|_/ / __ `/ //_/ _ \/ /_/ / / _ \______/ /_/ / /_/ / /_/ /
-         / /  / / /_/ / ,< /  __/ __/ / /  __/_____/ ____/ __  / ____/ 
-        /_/  /_/\__,_/_/|_|\___/_/ /_/_/\___/     /_/   /_/ /_/_/      
-                                                                       
---------------------------------------------------------------------------------
-        Makefile for Laravel projects
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+## How it looks
+
+```
+                                                                 
+██████╗ ██╗  ██╗██████╗     ███╗   ███╗ █████╗ ██╗  ██╗███████╗
+██╔══██╗██║  ██║██╔══██╗    ████╗ ████║██╔══██╗██║ ██╔╝██╔════╝
+██████╔╝███████║██████╔╝    ██╔████╔██║███████║█████╔╝ █████╗  
+██╔═══╝ ██╔══██║██╔═══╝     ██║╚██╔╝██║██╔══██║██╔═██╗ ██╔══╝  
+██║     ██║  ██║██║         ██║ ╚═╝ ██║██║  ██║██║  ██╗███████╗
+╚═╝     ╚═╝  ╚═╝╚═╝         ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+                                                                 
 Usage:
-  make [target] [arguments]
+  make -f php.mk <target> <target options>
 
-Arguments:
-  testName        for all test runners - Filter which tests to run
+Example:
+  make -f php.mk phpcs
+  make -f php.mk phpmd ./App
 
 Targets:
 
   ---             --------------------------------------------------------------
-  install         Install all PHP Tools (skip existing tools in ./vendor/bin)
-  check-tools     Check all Tools
-  ---             --------------------------------------------------------------
-  unit            Run Unit tests
-  feature         Run Feature tests
-  integration     Run Integration tests
-  tests           Run all tests
-  coverage        Run all tests with Code Coverage report
-  infection       Run Mutation Testing
-  ---             --------------------------------------------------------------
-  metrics         Generate Code Metrics Report project
-  ---             --------------------------------------------------------------
   phpcs           Check Code Style with PHP CodeSniffer [opt.: path]
   phpmd           Check Code Style with PHP MessDetector [opt.: path]
-  phpstan         Analyze Project with PHPStan [opt.: path]
   ---             --------------------------------------------------------------
   help            Show this help and exit
 
@@ -50,12 +38,7 @@ Targets:
 
 ## Features:
 
-- Install PHP tools
-- Check tools
-- Run Unit, Feature, Integration Tests with PHPUnit
-- Run Mutation Tests with Infection
-- Analyze project with PHPStan, CodeSniffer, Mess Detector
-- Collect PHP Metrics
-- Generate Code Coverage report
+- Run `PHP Mess Detector` from your project `vendor/bin` directory using your `phpmd.xml` or `phpmd.xml.dist`
+- Run `PHP CodeSniffer` from your project `vendor/bin` directory using your `phpcs.xml` or `phpcs.xml.dist`
 
 (*) under development
---
+++ php.mk
@@ -0,0 +1,109 @@
+# ==============================================================================
+# This Makefile helps to run various php tools for current project
+#
+# How-to:
+#   1. copy `php.mk` to root of your project
+#   2. add `php.mk` to your local or global gitignore
+#   3. make -f php.mk <target> <options>
+# ==============================================================================
+
+.SILENT: ;               # no need for @
+.ONESHELL: ;             # recipes execute in same shell
+.NOTPARALLEL: ;          # wait for this target to finish
+.EXPORT_ALL_VARIABLES: ; # send all vars to shell
+
+
+# http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=PHP%20make
+define LOGO
+                                                                 
+██████╗ ██╗  ██╗██████╗     ███╗   ███╗ █████╗ ██╗  ██╗███████╗
+██╔══██╗██║  ██║██╔══██╗    ████╗ ████║██╔══██╗██║ ██╔╝██╔════╝
+██████╔╝███████║██████╔╝    ██╔████╔██║███████║█████╔╝ █████╗  
+██╔═══╝ ██╔══██║██╔═══╝     ██║╚██╔╝██║██╔══██║██╔═██╗ ██╔══╝  
+██║     ██║  ██║██║         ██║ ╚═╝ ██║██║  ██║██║  ██╗███████╗
+╚═╝     ╚═╝  ╚═╝╚═╝         ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+                                                                 
+endef
+
+################################################################################
+# Colors
+# ##############################################################################
+Black        := $(shell tput -Txterm setaf 0)
+Red          := $(shell tput -Txterm setaf 1)
+Green        := $(shell tput -Txterm setaf 2)
+Yellow       := $(shell tput -Txterm setaf 3)
+LightPurple  := $(shell tput -Txterm setaf 4)
+Purple       := $(shell tput -Txterm setaf 5)
+Blue         := $(shell tput -Txterm setaf 6)
+White        := $(shell tput -Txterm setaf 7)
+NC           := $(shell tput -Txterm sgr0)
+################################################################################
+
+
+SHELL := /bin/bash
+comma  = ,
+space := # <- This Whitespace is Necessary for Substitution
+space +=
+
+# Run this makefile help by default
+.DEFAULT_GOAL = help
+
+# Determine this Makefile as Main file
+THIS_MAKEFILE := $(word $(words $(MAKEFILE_LIST)), $(MAKEFILE_LIST))
+ARGS := $(filter-out $@, $(MAKECMDGOALS))
+# Current Working Dir of Makefile (Full path)
+CWD := $(shell cd $(shell dirname $(THIS_MAKEFILE)); pwd)
+# Project dir (always current dir from which runs this makefile)
+PROJECT_DIR := $(CURDIR)
+REPORT_PATH := $(PROJECT_DIR)/storage/logs
+
+## Project specific dirs:
+VENDOR_BIN := $(PROJECT_DIR)/vendor/bin
+
+## Tools binary path
+PHPCS := $(VENDOR_BIN)/phpcs
+PHPMD := $(VENDOR_BIN)/phpmd
+
+## Tools configuration files:
+PHPMD_CONFIG := $(shell find -f phpmd.xml* 2>/dev/null | head -n 1)
+PHPMD_RULES  := $(if $(PHPMD_CONFIG), $(PROJECT_DIR)/$(PHPMD_CONFIG), "cleancode,codesize,controversial,design,naming,unusedcode")
+
+## Filters for makefile targets:
+CHANGED_FILES = $(shell git diff --name-only --diff-filter=ACMR $(BRANCH_BASE) HEAD | grep \.php | paste -sd "," -)
+PHPCS_FILTER  = $(if $(filter-out $@, $(ARGS)), $(filter-out $@, $(ARGS)), $(subst $(comma),$(space),$(CHANGED_FILES)))
+PHPMD_FILTER  = $(if $(filter-out $@, $(ARGS)), $(filter-out $@, $(ARGS)), $(CHANGED_FILES))
+
+.PHONY: help
+---: ## --------------------------------------------------------------
+phpcs: ## Check Code Style with PHP CodeSniffer [opt.: path]
+	$(PHPCS) --version
+	$(PHPCS) \
+	--report-full=$(REPORT_PATH)/phpcs_report.log \
+	--report-diff --report-full $(PHPCS_FILTER) \
+	&& echo "$(Green)SUCCSESS!$(NC)" || { echo "$(Red)FAILURE!$(NC)\n${PHPCS_LOG}"; exit 1;}
+
+phpmd: ## Check Code Style with PHP MessDetector [opt.: path]
+	$(PHPMD) --version
+	echo "config: $(PHPMD_RULES)"
+	$(PHPMD) $(PHPMD_FILTER) text $(PHPMD_RULES) \
+	--reportfile $(REPORT_PATH)/phpmd_report.txt \
+	&& echo "$(Green)SUCCSESS!$(NC)" \
+	|| (sed 's~$(CURDIR)~.~g' $(REPORT_PATH)/phpmd_report.txt && echo "\n$(Red)FAILURE!$(NC)")
+
+---: ## --------------------------------------------------------------
+help: ## Show this help and exit
+	echo "$${LOGO}"
+	echo "Usage:"
+	echo "  make -f $(THIS_MAKEFILE) <target> <target options>"
+	echo ''
+	echo "Example:"
+	echo "  make -f $(THIS_MAKEFILE) phpcs"
+	echo "  make -f $(THIS_MAKEFILE) phpmd ./App"
+	echo ''
+	echo "Targets:"
+	echo ''
+	grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(THIS_MAKEFILE) | awk 'BEGIN {FS = ":.*?## "}; \
+		{printf "  %-15s %s\n", $$1, $$2}'
+	echo ''
+%:
+	@:
